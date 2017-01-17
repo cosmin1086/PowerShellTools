4ed546a8e91997d844920eb88d80585a0c05d87e
cosmin1086 <cosmin1086@gmail.com>
2017-01-17 15:36:28 -0500

    Add files via upload

---
+++ Download&Compare.ps1
@@ -0,0 +1,11 @@
+﻿wget "https://...etl.jar" -outfile "C:\ETL\downloaded\etl.jar"
+if ((Get-FileHash "C:\ETL\downloaded\etl.jar").hash  -ne (Get-FileHash "C:\ETL\etl.jar").hash)
+    {
+    Copy-Item "c:\ETL\downloaded\etl.jar" "c:\ETL\etl.jar"
+    }
+else {
+    exit
+    }
+    
+
+
